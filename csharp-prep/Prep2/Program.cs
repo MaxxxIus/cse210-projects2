50e14c427ee70e4a08acdcf86ddd8cb815842480
Maxxxlus <Hatineer@gmail.com>
2026-01-15 17:52:14 -0700

    Replaced code with assignment code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade? ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+
+        if (gradeInt >= 90)
+        {
+            grade = "A";
+        } else if (gradeInt >= 80)
+        {
+            grade = "B";
+        } else if (gradeInt >= 70)
+        {
+            grade = "C";
+        } else if (gradeInt >= 60)
+        {
+            grade = "D";
+        } else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your Letter Grade: {grade}");
+
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("Congrates!! You Passes!!!");
+        } else
+        {
+            Console.WriteLine("Sorry, You Failed. See you Next Semester.");
+        }
     }
 }
