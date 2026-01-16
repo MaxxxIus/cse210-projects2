bab180cac2ac9eb787477a8bea1f59c5e716ab8a
Maxxxlus <Hatineer@gmail.com>
2026-01-15 18:34:46 -0700

    replaced code with assignment code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Diagnostics;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int answer;
+
+        do
+        {
+            Console.Write("What is your guess between 1 and 100? ");
+            answer = int.Parse(Console.ReadLine());
+
+            if (answer < number)
+            {
+                Console.WriteLine("Higher");
+            } else if (answer > number)
+            {
+                Console.WriteLine("Lower");
+            }   else if (answer == number)
+            {
+                Console.WriteLine("Thats Correct!!!");
+            } else
+            {
+                Console.WriteLine("Invalide Response");
+            }
+        } while (answer != number);
     }
 }
