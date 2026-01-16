30f1b27bf93d577dcb7f559d91deb8c70b1b012b
Maxxxlus <Hatineer@gmail.com>
2026-01-15 19:22:23 -0700

    did assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int answer;
+        do
+        {
+            Console.WriteLine("Please give a Number");
+            Console.Write("Type 0 to stop: ");
+            answer = int.Parse(Console.ReadLine());
+            if (answer != 0)
+            {
+                numbers.Add(answer);
+            }
+
+        } while (answer != 0);
+
+        int sum = 0;
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (largest < number)
+            {
+                largest = number;
+            }
+        }
+        Console.WriteLine($"Sum: {sum}");
+        Console.WriteLine($"Average: {((float)sum) / numbers.Count}");
+        Console.WriteLine($"Maximum: {largest}");
+
+
+
+
     }
+
+    
+
+
 }
