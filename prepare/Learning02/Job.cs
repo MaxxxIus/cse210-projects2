dcc98929cfeec2f58301982ca30c9e2e8c2634d8
Maxxxlus <Hatineer@gmail.com>
2026-01-22 16:33:19 -0700

    Did the assignment

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,12 @@
+public class Job{
+    //attributes
+    public string _company;
+    public string _jobTitle;
+    public string _startYear;
+    public string _endYear;
+    //methods
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
